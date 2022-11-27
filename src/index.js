111c268c3bc81f46e3b5f38b361ac09aa6a7a2d7
youngsterjaidev <jaidevv999@gmail.com>
2022-11-27 16:18:27 +0000

    Update firebase.tsx and index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-import { config } from "dotenv"
+import { config } from "dotenv";
 import React, { useEffect, useState, Suspense } from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
@@ -14,7 +14,14 @@ import { ThemeProvider } from "styled-components";
 
 import ReactMapbox from "./components/map/ReactMapbox";
 
-import { Profile, Login, Loading, CreateAccount, PasswordReset, Home } from "./pages";
+import {
+  Profile,
+  Login,
+  Loading,
+  CreateAccount,
+  PasswordReset,
+  Home,
+} from "./pages";
 import styled from "styled-components";
 
 import {
@@ -25,23 +32,23 @@ import {
 } from "./custom-hooks";
 import UserContext from "./UserContext.js";
 
-config()
+config();
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
-const firebaseConfig = {
-  apiKey: "AIzaSyAa9TwCMfkEwsuaxx1yIbmVdK0fYhxCRR8",
-  authDomain: "alaric-339008.firebaseapp.com",
-  databaseURL: "https://alaric-339008-default-rtdb.firebaseio.com",
-  projectId: "alaric-339008",
-  storageBucket: "alaric-339008.appspot.com",
-  messagingSenderId: "264727266576",
-  appId: "1:264727266576:web:10bb6e7980b695c701d195",
-  measurementId: "G-V0ZQ4YTKPT"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAa9TwCMfkEwsuaxx1yIbmVdK0fYhxCRR8",
+//   authDomain: "alaric-339008.firebaseapp.com",
+//   databaseURL: "https://alaric-339008-default-rtdb.firebaseio.com",
+//   projectId: "alaric-339008",
+//   storageBucket: "alaric-339008.appspot.com",
+//   messagingSenderId: "264727266576",
+//   appId: "1:264727266576:web:10bb6e7980b695c701d195",
+//   measurementId: "G-V0ZQ4YTKPT"
+// };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// // Initialize Firebase
+// firebase.initializeApp(firebaseConfig);
 
 //let Login = () => <div>Login</div>
 //let Loading = () => <div>Loading</div>
